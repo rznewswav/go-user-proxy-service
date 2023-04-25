3599d1c35d445c6a13a965245bc498cc39a74370
raflie <raflie@newswav.com>
2023-04-25 14:55:47 +0800

    Exclude mysql from services

--- services/app/service.go
+++ services/app/service.go
@@ -2,7 +2,7 @@ package app
 
 import (
 	"service/services/health"
-	"service/services/mysql"
+	// "service/services/mysql"
 	"service/services/redis"
 	"service/services/server"
 )
@@ -11,10 +11,10 @@ func Start() {
 	shutdownHandler := InitShutdownHook()
 	onFinishShutdown := InitSignal(shutdownHandler)
 
-	mysql.Connect(shutdownHandler)
+	// mysql.Connect(shutdownHandler)
 	redis.Connect(shutdownHandler)
-	health.NewPatient("mysql", mysql.Health)
-	health.NewPatient("redis", redis.Health)
+	// health.NewPatient("db", mysql.Health)
+	health.NewPatient("cache", redis.Health)
 	server.Start(shutdownHandler)
 	shutdownHandler.Start(onFinishShutdown)
 }
