bfbd53e0fb7f0e26567f90c5e9e388b25e49ef75
raflie <raflie@newswav.com>
2023-04-26 17:26:29 +0800

    Add stack trace code

---
+++ cmd/stack/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"runtime"
+)
+
+type WithStackTrace interface {
+	StackTrace() errors.StackTrace
+}
+
+func fileLine(f errors.Frame) string {
+	fn := runtime.FuncForPC(uintptr(f) - 1)
+	if fn == nil {
+		return "unknown:0"
+	}
+	file, line := fn.FileLine(uintptr(f) - 1)
+	return fmt.Sprintf("%s:%d", file, line)
+
+}
+
+func main() {
+	err := errors.WithStack(errors.New("hello"))
+	if errorWithStack, castable := err.(WithStackTrace); castable {
+		stackTraces := errorWithStack.StackTrace()
+		for _, trace := range stackTraces {
+			println(fileLine(trace))
+		}
+	}
+}
