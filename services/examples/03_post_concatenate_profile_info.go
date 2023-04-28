9662b6a7e0d72bfac4cfc27e5286e3c5ac854feb
raflie <raflie@newswav.com>
2023-04-28 15:06:48 +0800

    Add samples for translation key

--- services/examples/03_post_concatenate_profile_info.go
+++ services/examples/03_post_concatenate_profile_info.go
@@ -9,15 +9,11 @@ import (
 	"service/services/server/resp"
 )
 
-type ConcateRequestBody struct {
-	Age int `json:"age" binding:"required"`
-}
-
 var ConcatenateProfileInfo = controllers.
-	Post[ConcateRequestBody]("/api/v1/me").
+	Post[map[string]interface{}]("/api/v1/me").
 	UseMiddleware(&middlewares.AuthMiddleware).
 	Handle(func(
-		request req.Request[ConcateRequestBody],
+		request req.Request[map[string]interface{}],
 	) (Response resp.Response) {
 		body := request.Body()
 		profile, _ := request.Get(constants.UserProfile)
